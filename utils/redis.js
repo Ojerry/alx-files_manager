add8edbca58313b9e3d6930fbf8bce476392723f
Ojerry <onotu128@gmail.com>
2023-04-07 21:00:59 +0100

    new commit

--- utils/redis.js
+++ utils/redis.js
@@ -1,30 +1,30 @@
-import redis from "redis";
+import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.myClient = redis.createClient();
-        this.myClient.on('error', (error) => console.log(error));
-    }
+  constructor() {
+    this.myClient = createClient();
+    this.myClient.on('error', (error) => console.log(error));
+  }
 
-    isAlive() {
-        return this.myClient.connected;
-    }
+  isAlive() {
+    return this.myClient.connected;
+  }
 
-    async get(key) {
-        const asyncGet = promisify(this.myClient.GET).bind(this.myClient);
-        return asyncGet(key);
-    }
+  async get(key) {
+    const asyncGet = promisify(this.myClient.GET).bind(this.myClient);
+    return asyncGet(key);
+  }
 
-    async set(key, val, time) {
-        const setAsync = promisify(this.myClient.SET).bind(this.myClient);
-        return setAsync(key, val, 'EX', time);
-    }
+  async set(key, val, time) {
+    const asyncSet = promisify(this.myClient.SET).bind(this.myClient);
+    return asyncSet(key, val, 'EX', time);
+  }
 
-    async del(key) {
-        const asyncDel = promisify(this.myClient.DEL).bind(this.myClient);
-        return asyncDel(key);
-    }
+  async del(key) {
+    const asyncDel = promisify(this.myClient.DEL).bind(this.myClient);
+    return asyncDel(key);
+  }
 }
 
 const redisClient = new RedisClient();
